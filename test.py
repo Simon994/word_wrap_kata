0efa0aeacffdc3b827fa392978ae6955e15f677c
SimonN <65023744+Simon994@users.noreply.github.com>
2021-02-20 16:24:39 +0000

    Add tests and working wrap function

--- test.py
+++ test.py
@@ -14,6 +14,38 @@ class TestWrapper(unittest.TestCase):
     def test_wrapper_returns_string_when_shorter_than_column_length(self):
         wrapped_result = self.wrapper.wrap('beans', 6)
         self.assertEqual('beans', wrapped_result)
+    
+    def test_wrapper_returns_string_when_length_is_equal_to_column_length(self):
+        wrapped_result = self.wrapper.wrap('beans', 5)
+        self.assertEqual('beans', wrapped_result)
+
+    def test_wrapper_wraps_string_with_no_spaces_and_longer_than_column_length(self):
+        wrapped_result = self.wrapper.wrap('bean', 2)
+        self.assertEqual('be\nan', wrapped_result)
+
+    def test_wrapper_wraps_another_string_with_no_spaces_and_longer_than_column_length(self):
+        wrapped_result = self.wrapper.wrap('andgone', 3)
+        self.assertEqual('and\ngon\ne', wrapped_result)
+
+    def test_wrapper_wraps_two_words_only_space(self):
+        wrapped_result = self.wrapper.wrap('fava beans', 6)
+        self.assertEqual('fava\nbeans', wrapped_result)
+    
+    def test_wrapper_wraps_two_more_words_with_only_one_space(self):
+        wrapped_result = self.wrapper.wrap('bean beangone', 10)
+        self.assertEqual('bean\nbeangone', wrapped_result)
+
+    def test_wrapper_wraps_three_words_at_all_spaces(self):
+        wrapped_result = self.wrapper.wrap('bean bean bean', 6)
+        self.assertEqual('bean\nbean\nbean', wrapped_result)
+    
+    def test_wrapper_wraps_three_words_at_second_space(self):
+        wrapped_result = self.wrapper.wrap('bean bean gone', 10)
+        self.assertEqual('bean bean\ngone', wrapped_result)
+    
+    def test_wrapper_wraps_two_words_with_second_word_longer_than_column_length(self):
+        wrapped_result = self.wrapper.wrap('bean beanandgone', 10)
+        self.assertEqual('bean beana\nndgone', wrapped_result)
 
 if __name__ == '__main__':
     unittest.main()
--- word_wrap/__init__.py
+++ word_wrap/__init__.py
@@ -1,3 +1,26 @@
 class Wrapper:
     def wrap(self, string, column_length):
-        return ''
+        # if length of string is less than column_length, simply return string
+        if len(string) <= column_length:
+            return string
+        
+        #Between 0 and column_length, find the highest index where there is a space
+        #(if no space found, rfind will return -1)
+        breakPoint = string.rfind(" ", 0, column_length+1)
+        
+        # find the length of the next word in the string
+        next_word_length = 0
+        afterBreak = breakPoint+1
+        if " " in string[afterBreak:]:
+            next_word_length = string[afterBreak:].find(" ")
+        else:
+            next_word_length = len(string[afterBreak:])
+
+        if breakPoint == -1 or next_word_length > column_length:
+            breakPoint = column_length
+        
+        # store the line then recursively call wrap,
+        # with string sliced from breakpoint to end of string
+        line = string[:breakPoint] + "\n"
+        remaining_text = string[breakPoint:].strip()
+        return line + self.wrap(remaining_text, column_length)
